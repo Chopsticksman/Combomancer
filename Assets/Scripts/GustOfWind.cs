0e548bbe96e76bd3f9ab87ff0bfabd00d53ac6c3
Yoav Gutman <ygutman@ucsd.edu>
2025-11-22 16:52:20 -0800

    fixed gustofwind

--- Assets/Scripts/GustOfWind.cs
+++ Assets/Scripts/GustOfWind.cs
@@ -5,9 +5,8 @@ public class GustOfWind : MonoBehaviour
 	int pushBack = 3;
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		Enemy hit = EnemyManager.instance(collision.gameObject);
+		Enemy hit = EnemyManager.instance.TryGetEnemy(collision.gameObject);
 		if (hit != null)
-			hit.transform.position.x += pushBack;
-		}
+			hit.transform.position += new Vector3(pushBack, 0, 0);
 	}
 }
